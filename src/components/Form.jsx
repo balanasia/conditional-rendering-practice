105d8044e172f69516e0463c26215282efba5378
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-21 16:15:12 -0500

    initial commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
 function Form() {
   return (
     <form className="form">
