6f884a06b06727aba2e1eb5ada415fbb3c6f1932
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-21 16:30:12 -0500

    Fix the solution without using Login.jsx and instead solving it logically

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-function Form() {
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {props.userIsRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">
+        {props.userIsRegistered ? "Register" : "Login"}
+      </button>
     </form>
   );
 }
